f2f4ba283dbc036baa596c9af6e44f995efe577c
Caio Azevedo <136472218+justCaioDev@users.noreply.github.com>
2024-05-06 16:11:55 -0300

    feat: primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+const projects = [
+    {
+        nome: "IniciDev",
+        tecnologias: [
+            'JavaScript',
+            'HTML',
+            'CSS'
+        ],
+        url: 'https://justcaiodev.github.io/inicidev/',
+        img: ''
+    },
+    {
+        nome: "Studio Ghibli Filmes",
+        tecnologias: [
+            'JavaScript',
+            'HTML',
+            'CSS'
+        ],
+        url: 'https://justcaiodev.github.io/ghibli-films/',
+        img: ''
+    },
+    {
+        nome: "Lorem Social",
+        tecnologias: [
+            'JavaScript',
+            'HTML',
+            'CSS'
+        ],
+        url: 'https://justcaiodev.github.io/Lorem-Social/',
+        img: ''
+    },
+    {
+        nome: "Quiz Harry Potter",
+        tecnologias: [
+            'JavaScript',
+            'HTML',
+            'CSS'
+        ],
+        url: 'https://justcaiodev.github.io/quiz-nlw-rocketseat/',
+        img: ''
+    }
+]
