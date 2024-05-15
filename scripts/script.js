bee39d9b7a140c57707099456f6c3b0e0bff920b
Caio Azevedo <136472218+justCaioDev@users.noreply.github.com>
2024-05-15 15:15:56 -0300

    feat: add a function that generates the images in the row projects

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,12 @@
 const projects = [
     {
-        nome: "IniciDev",
+        nome: "Lorem GYM",
         tecnologias: [
-            'JavaScript',
             'HTML',
             'CSS'
         ],
-        url: 'https://justcaiodev.github.io/inicidev/',
-        img: ''
-    },
-    {
-        nome: "Studio Ghibli Filmes",
-        tecnologias: [
-            'JavaScript',
-            'HTML',
-            'CSS'
-        ],
-        url: 'https://justcaiodev.github.io/ghibli-films/',
-        img: ''
+        url: 'https://justcaiodev.github.io/lorem-gym/',
+        img: '../assets/banner-lorem-gym.png'
     },
     {
         nome: "Lorem Social",
@@ -27,7 +16,17 @@ const projects = [
             'CSS'
         ],
         url: 'https://justcaiodev.github.io/Lorem-Social/',
-        img: ''
+        img: '../assets/banner-lorem-social.png'
+    },
+    {
+        nome: "Studio Ghibli Filmes",
+        tecnologias: [
+            'JavaScript',
+            'HTML',
+            'CSS'
+        ],
+        url: 'https://justcaiodev.github.io/ghibli-films/',
+        img: '../assets/banner-studio-ghibli.png'
     },
     {
         nome: "Quiz Harry Potter",
@@ -37,6 +36,28 @@ const projects = [
             'CSS'
         ],
         url: 'https://justcaiodev.github.io/quiz-nlw-rocketseat/',
-        img: ''
+        img: '../assets/banner-quizz-harry-potter.png'
+    },
+    {
+        nome: "IniciDev",
+        tecnologias: [
+            'JavaScript',
+            'HTML',
+            'CSS'
+        ],
+        url: 'https://justcaiodev.github.io/inicidev/',
+        img: '../assets/banner-inicidev.png'
     }
 ]
+
+function rowProjects() {
+    const row = document.querySelector('.row')
+
+    for (let i = 0; i < projects.length; i++) {
+        const img = document.createElement('img')
+        img.setAttribute('src', projects[i].img)
+        row.appendChild(img)
+    }
+}
+
+rowProjects()
